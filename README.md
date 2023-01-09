c627e06b39e9e4278bf4f14f8844b2ac57e2236c
eunjineee <yej6642@naver.com>
2023-01-10 01:17:15 +0900

    메세지 띄우기, 파일 업로드

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # JAVA
+
+### 참고 - 한코딩 [스트링부트] 게시판 무작정 따라하기
+
+https://www.youtube.com/watch?v=frI5CoZe-vE&list=PLZzruF3-_clsWF2aULPsUPomgolJ-idGJ
+
+
+
+### 개발 환경
+
+✨ 인텔리제이
+
+✨ MYSQL
+
+✨ MYSQL Workbench
+
+
+
+###  프로젝트 구성
+
+✨ DB 생성
+
+✨ 게시글 작성폼 생성
+
+✨ 글 작성 처리
+
+✨ 게시글 리스트
+
+✨ 게시글 상세 페이지
+
+✨ 게시글 삭제
+
+✨ 게시글 수정
+
+✨ 메세지 띄우기
+
+✨ 파일 업로드
+
+
+
--- board_study/board/src/main/java/com/study/board/Controller/BoardController.java
+++ board_study/board/src/main/java/com/study/board/Controller/BoardController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class BoardController {
@@ -26,10 +27,14 @@ public class BoardController {
     }
 
     @PostMapping("/board/writepro")
-    public String boardWritePro(Board board) {
+    public String boardWritePro(Board board, Model model, MultipartFile file) throws Exception{
 
-        boardService.write(board);
-        return "";
+        boardService.write(board, file);
+
+        model.addAttribute("message","글 작성이 완료되었습니다.");
+        model.addAttribute("searchUrl", "/board/list");
+
+        return "message";
     }
 
     @GetMapping("/board/list")
@@ -60,12 +65,12 @@ public class BoardController {
     }
 
     @PostMapping("/board/update/{id}")
-    public String boardUpdate(@PathVariable("id") Integer id, Board board) {
+    public String boardUpdate(@PathVariable("id") Integer id, Board board, MultipartFile file) throws Exception{
         Board boardTemp = boardService.boardView(id);
         boardTemp.setTitle(board.getTitle());
         boardTemp.setContent(board.getContent());
 
-        boardService.write(boardTemp);
+        boardService.write(boardTemp, file);
         return "redirect:/board/list";
     }
 }
