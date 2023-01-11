f894f26d0dec1d7fefc1498aa38c1334a0c488d3
eunjineee <yej6642@naver.com>
2023-01-11 22:06:55 +0900

    이것이 자바다 SocketClient

---
+++ This is JAVA/ch09/sec07/SocketClient.java
@@ -0,0 +1,34 @@
+package ch09.sec07;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class SocketClient {
+    public class SocketClient {
+        ChatServer chatServer;
+        Socket socket;
+        DataInputStream dis;
+        DataOutputStream dos;
+        String clientIp;
+        String chatName;
+    }
+
+}
+    //생성자
+    public SocketClient(ChatServer chatServer, Socket socket) {
+        try {
+            this.chatServer = chatServer;
+            this.socket = socket;
+            this.dis = new DataInputStream(socket.getInputStream());
+            this.dos = new DataOutputStream(socket.getOutputStream());
+            InetSocketAddress isa = (InetSocketAddress) socket.getRemoteSocketAddress();
+            this.clientIp = isa.getHostName();
+            receive();
+        } catch(IOException e) {
+
+        }
+    }
+}
